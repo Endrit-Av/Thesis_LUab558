68a9e2a9beba4d3a2e42795bc9ab3140c79d8dcf
Endrit Avdulli <endrit_avdulli@hotmail.com>
2024-12-23 13:34:46 +0100

    Implement footer component

--- thesis_luab558.client/src/app/app.module.ts
+++ thesis_luab558.client/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './services/mainpage/mainpage.component';
 import { HeaderComponent } from './services/header/header.component';
+import { FooterComponent } from './services/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainpageComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
